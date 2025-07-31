972339c4932f17a25df4b2a65c8df161bd6b877a
Zaynab <141653893+curiousConductor49@users.noreply.github.com>
2025-07-31 17:02:36 -0400

    Pseudocode functionality

---
+++ script.js
@@ -0,0 +1,25 @@
+// PSEUDOCODE (use as code guide and rework as comments later)
+
+// Function create grid
+    // Pass In: div num
+    // Action:
+        // ACCESS DOM 
+        // CREATE a grid of div elements inside the existing div container in the HTML using a loop
+    // Pass Out: n/a
+// Endfunction
+
+// Function change div colour
+    // Pass In: n/a
+    // Action: CHANGE the div colours when moused over using event listeners and DOM attributes
+    // Pass Out: n/a
+// Endfunction
+
+// Function create new grid w/ user input
+    // Pass In: div num
+    // Action: 
+        // OBTAIN user input
+        // REMOVE existing grid of divs inside div container from DOM
+        // CREATE new grid of divs using user input
+        // ATTACH to event listener on button
+    // Pass Out: n/a
+// Endfunction
